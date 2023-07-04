6dc7960888795216a956da193ecf40a6afa26d99
Oğuzhan Arslantaş <iamoguzhanarslantas@gmail.com>
2023-07-04 14:49:44 +0300

    add login by google account

--- lib/src/common/common.dart
+++ lib/src/common/common.dart
@@ -1,3 +1,4 @@
+export 'components/alert_dialog_with_text_button.dart';
 export 'constants/app_colors.dart';
 export 'logger/bilsame_logger.dart';
 export 'logger/global_logger.dart';
--- lib/src/login/pages/login_page.dart
+++ lib/src/login/pages/login_page.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+import 'package:bilsame/src/common/common.dart' show AlertDialogWithTextButton;
+import 'package:bilsame/src/config/config.dart' show AppConfig;
+import 'package:bilsame/src/login/login.dart' show loginControllerProvider;
+import 'package:bilsame/src/user/user.dart' show UserPage;
 
 class LoginPage extends ConsumerWidget {
   static const routeName = '/login';
@@ -7,6 +13,47 @@ class LoginPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    final loginController = ref.watch(loginControllerProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(AppConfig.appBarTitle),
+      ),
+      body: loginController.isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Center(
+              child: TextButton(
+                onPressed: () {
+                  ref
+                      .read(loginControllerProvider.notifier)
+                      .loginByGoogle()
+                      .then(
+                    (_) async {
+                      if (ref.read(loginControllerProvider).error == null) {
+                        context.go(UserPage.routeName);
+                      } else {
+                        await showDialog(
+                          barrierDismissible: false,
+                          context: context,
+                          builder: (context) => AlertDialogWithTextButton(
+                            title: 'Sign in Failed',
+                            content:
+                                'Could not sign in with Google. Please try again.',
+                            buttonTitle: 'OK',
+                            onPressed: () {
+                              context.pop();
+                            },
+                          ),
+                        );
+                        ref.invalidate(loginControllerProvider);
+                      }
+                    },
+                  );
+                },
+                child: const Text('Google Giris'),
+              ),
+            ),
+    );
   }
 }
