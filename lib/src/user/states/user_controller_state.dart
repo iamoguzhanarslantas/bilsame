21d3279eeb461310bcf0f07c02179f0ae49fbf94
Oğuzhan Arslantaş <iamoguzhanarslantas@gmail.com>
2023-07-04 14:50:26 +0300

    fix

--- lib/src/user/states/user_controller_state.dart
+++ lib/src/user/states/user_controller_state.dart
@@ -3,7 +3,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'user_controller_state.freezed.dart';
 
 @freezed
-class UserControllerState with _$UserControllerState{
+class UserControllerState with _$UserControllerState {
   const factory UserControllerState({
     required String? name,
     required String? email,
