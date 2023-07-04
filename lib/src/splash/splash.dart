4a156c9ba01a88e754952cad09d47c625258d109
Oğuzhan Arslantaş <iamoguzhanarslantas@gmail.com>
2023-07-04 12:15:03 +0300

    configure firebase and add splash states

--- lib/src/splash/splash.dart
+++ lib/src/splash/splash.dart
@@ -1,1 +1,3 @@
 export 'pages/splash_page.dart';
+export 'states/splash_page_state.dart';
+export 'states/splash_page_state_notifier.dart';
---
+++ lib/src/splash/states/splash_page_state_notifier.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:bilsame/firebase_options.dart' show DefaultFirebaseOptions;
+import 'package:bilsame/src/common/common.dart' show logger;
+import 'package:bilsame/src/splash/splash.dart' show SplashPageState;
+
+class SplashPageStateNotifier extends StateNotifier<SplashPageState> {
+  final Ref ref;
+  SplashPageStateNotifier({required this.ref}) : super(SplashPageState.init());
+  Future<void> initializeApp() async {
+    try {
+      logger.i('initializeApp');
+      state = SplashPageState.init();
+      await Firebase.initializeApp(
+          name: 'DrBilsem', options: DefaultFirebaseOptions.currentPlatform);
+
+      state = state.copyWith(appInitialized: true);
+    } catch (e) {
+      logger.e(e);
+    }
+  }
+}
